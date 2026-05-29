e808598ed6761d82542156c7ef82382d0ed5d0ac
Shaurya Kumar <shaurya.kumar.mn@gmail.com>
2026-05-28 20:15:30 -0500

    envirnment setup

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+import os
+load_dotenv()
+
+llm = ChatGroq(model="llama-3.3-70b-versatile")
+
 def main():
     print("Hello from langchain-practice!")
+    print(os.getenv("GROQ_API_KEY"))
 
 
 if __name__ == "__main__":
